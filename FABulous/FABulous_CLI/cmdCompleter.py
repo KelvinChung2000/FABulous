d6db4830a522d5a197b765bbf428268966710bfe
King Lok Chung <king.chung@student.manchester.ac.uk>
2024-12-20 17:41:14 +0000

    Moving towards cmd2

---
+++ FABulous/FABulous_CLI/cmdCompleter.py
@@ -0,0 +1,10 @@
+import argparse
+
+from cmd2 import Cmd
+
+# class FABulousCompleter(Cmd):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+
+#     def tile_complete(self) -> list[str]:
+#         pass

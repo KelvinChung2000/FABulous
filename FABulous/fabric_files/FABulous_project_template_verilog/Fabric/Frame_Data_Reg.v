db98aca6efc447fef270ced7fd86020b52bf6901
Kelvin Chung <king.chung@manchester.ac.uk>
2025-09-02 12:13:02 +0100

    chore: linting files

--- FABulous/fabric_files/FABulous_project_template_verilog/Fabric/Frame_Data_Reg.v
+++ FABulous/fabric_files/FABulous_project_template_verilog/Fabric/Frame_Data_Reg.v
@@ -1,14 +1,18 @@
-module Frame_Data_Reg (FrameData_I, FrameData_O, RowSelect, CLK);
-    parameter FrameBitsPerRow = 32;
-    parameter RowSelectWidth = 5;
-    parameter Row = 1;
-    input [FrameBitsPerRow-1:0] FrameData_I;
-    output reg [FrameBitsPerRow-1:0] FrameData_O;
-    input [RowSelectWidth-1:0] RowSelect;
-    input CLK;
+module Frame_Data_Reg (
+    FrameData_I,
+    FrameData_O,
+    RowSelect,
+    CLK
+);
+  parameter integer FrameBitsPerRow = 32;
+  parameter integer RowSelectWidth = 5;
+  parameter integer Row = 1;
+  input reg [FrameBitsPerRow-1:0] FrameData_I;
+  output reg [FrameBitsPerRow-1:0] FrameData_O;
+  input reg [RowSelectWidth-1:0] RowSelect;
+  input CLK;
 
-    always @ (posedge CLK) begin
-        if (RowSelect==Row)
-            FrameData_O <= FrameData_I;
-    end//CLK
+  always @(posedge CLK) begin
+    if (RowSelect == Row) FrameData_O <= FrameData_I;
+  end  //CLK
 endmodule

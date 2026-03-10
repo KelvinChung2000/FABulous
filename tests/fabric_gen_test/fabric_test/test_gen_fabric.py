2ab30e1602e71955ccc9e3c8e41cf068dbadc187
Kelvin Chung <king.chung@manchester.ac.uk>
2026-03-10 17:46:54 +0000

    chore: clean to simple test

--- tests/fabric_gen_test/fabric_test/test_gen_fabric.py
+++ tests/fabric_gen_test/fabric_test/test_gen_fabric.py
@@ -9,14 +9,10 @@ from fabulous.fabric_generator.code_generator.code_generator import CodeGenerato
 from fabulous.fabric_generator.gen_fabric.gen_fabric import generateFabric
 
 
-@pytest.mark.parametrize("fabric_name", ["eFPGA", "myCustomFabric", "test_FPGA_v2"])
-def test_generate_fabric_uses_fabric_name(
-    fabric_name: str,
-    mocker: MockerFixture,
-) -> None:
+def test_generate_fabric_uses_fabric_name(mocker: MockerFixture) -> None:
     """generateFabric should use fabric.name as the module name."""
     fabric = mocker.create_autospec(Fabric)
-    fabric.name = fabric_name
+    fabric.name = "test_fabric"
     fabric.tile = []
     fabric.configBitMode = ConfigBitMode.FLIPFLOP_CHAIN
     fabric.maxFramesPerCol = 20
@@ -28,4 +24,4 @@ def test_generate_fabric_uses_fabric_name(
 
     generateFabric(writer, fabric)
 
-    writer.addHeader.assert_called_once_with(fabric_name)
+    writer.addHeader.assert_called_once_with("test_fabric")

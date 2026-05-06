10695bfd4db11a80a64628a53b39f3b6debde411
Kelvin Chung <king.chung@manchester.ac.uk>
2026-05-06 14:32:40 +0100

    chore: comment

--- fabulous/fabric_generator/parser/parse_csv.py
+++ fabulous/fabric_generator/parser/parse_csv.py
@@ -456,7 +456,7 @@ def parseTilesCSV(fileName: Path) -> tuple[list[Tile], list[tuple[str, str]]]:
                 if muxSize >= 2:
                     configBit += (muxSize - 1).bit_length()
 
-        # sorting to ensure the ports section is always in the same order independent
+        # sorting to ensure the ports section is always in the same order
         ports.sort(key=lambda p: p.wireDirection)
 
         new_tiles.append(
